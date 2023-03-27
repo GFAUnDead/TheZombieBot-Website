41fa9ee4344adcaa4d1a6a16244c8ede8b3e81fb
GFAUnDead <hi@simdifrandomhd.xyz>
2023-03-27 13:42:05 +1100

    Update index.php

--- API/index.php
+++ API/index.php
@@ -8,13 +8,22 @@
     <div class="container">
       <h1>YourBotsOnline API</h1>
       <p>This website is currently holding the following APIs:</p>
-      <ul>
-        <li class="api" data-name="Time">Time</li>
-        <li class="api" data-name="Weather">Weather</li>
-        <li class="api" data-name="Todo list">Todo list</li>
-      </ul>
+      <div class="api-buttons">
+        <button class="api" data-name="Time">Time</button>
+        <button class="api" data-name="Weather">Weather</button>
+        <button class="api" data-name="Todo list">Todo list</button>
+      </div>
       <div class="api-info"></div>
     </div>
+
+    <?php
+      // Define an array with the API information
+      $apiInfo = array(
+        'Time' => 'This API provides the current time in various timezones.',
+        'Weather' => 'This API provides the current weather conditions for a given location.',
+        'Todo list' => 'This API allows users to create and manage todo lists.'
+      );
+    ?>
 
     <script>
       const apis = document.querySelectorAll('.api');
@@ -22,23 +31,9 @@
       for (let api of apis) {
         api.addEventListener('click', function() {
           const apiName = this.getAttribute('data-name');
-          const apiInfo = getApiInfo(apiName);
+          const apiInfo = '<?php echo $apiInfo[' + apiName + ']; ?>';
           showApiInfo(apiInfo);
         });
-      }
-
-      function getApiInfo(apiName) {
-        // This function should return the information about the given API name
-        switch (apiName) {
-          case 'Time':
-            return 'This API provides the current time in various timezones.';
-          case 'Weather':
-            return 'This API provides the current weather conditions for a given location.';
-          case 'Todo list':
-            return 'This API allows users to create and manage todo lists.';
-          default:
-            return '';
-        }
       }
 
       function showApiInfo(apiInfo) {
