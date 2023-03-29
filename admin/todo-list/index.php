f9ff91e383dfb9465214cddb5961ab78eee0f7a8
SimDifRandomHD <lachlan@lochstudios.com>
2023-03-29 19:25:19 +1100

    Update index.php

--- admin/todo-list/index.php
+++ admin/todo-list/index.php
@@ -33,85 +33,83 @@
                     echo "API key is required.";
                     exit();
                 }
-
+                
                 // Connect to the database
                 $servername = "(REDACTED)";
                 $username = "(REDACTED)";
                 $password = "(REDACTED)";
                 $dbname = "(REDACTED)";
-
+                
                 $conn = new mysqli($servername, $username, $password, $dbname);
-
+                
                 // Check if the connection is successful
                 if ($conn->connect_error) {
                     die("Connection failed: " . $conn->connect_error);
                 }
-
+                
                 // Prepare the SQL statement to retrieve the channel name and username for the given API key
                 $stmt = $conn->prepare("SELECT channelname FROM allowed_users WHERE api_key = ?");
                 $stmt->bind_param("s", $api_key);
-
+                
                 // Execute the SQL statement
                 $stmt->execute();
-
+                
                 // Bind the result to variables
                 $stmt->bind_result($channelname);
-
+                
                 // Fetch the result
                 $stmt->fetch();
-
+                
                 // Close the statement
                 $stmt->close();
-
+                
                 // Close the database connection
                 $conn->close();
-
+                
                 // Check if the provided API key is valid and retrieve the channel name from the database
                 if (empty($channelname)) {
                     // Return an error message if the API key is not valid
                     echo "Invalid API key.";
                     exit();
                 }
-
+                
                 // Check if the channel name is specified in the URL
                 if (isset($_GET['channel'])) {
-                    // Retrieve the API key from the URL
-                    $api_key = $_GET['channel'];
+                    // Retrieve the channel name from the URL and store it in a variable
+                    $channel_name = $_GET['channel'];
                 } else {
-                    // Return an error message if the API key is not specified in the URL
+                    // Return an error message if the channel name is not specified in the URL
                     echo "Your channel name is required.";
                     exit();
                 }
-
+                
                 // Connect to the database
-				include('db_connect.php');
-				
-				$conn = new mysqli($servername, $username, $password, $dbname);
-				if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-				}
-				
-				// Retrieve the data from the table
-				$sql = "SELECT todo_text FROM todos WHERE channel_id IN (SELECT id FROM channels WHERE name='". $_GET[channel] ."')";
-				$result = $conn->query($sql);
+                include('db_connect.php');
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                
+                // Retrieve the data from the table for the specified channel name and channel owner
+                $sql = "SELECT todo_text FROM todos WHERE channel_id IN (SELECT id FROM channels WHERE name='$channel_name' AND owner='$channelname')";
+                $result = $conn->query($sql);
+                
 				
                 // Display text for user
-                echo "<h2>Viewing all available tasks on this page for $_GET[channel]:</h2>";
+                echo "<h2>Viewing all available tasks on this page for $_GET[channel]:</h2>\r\n";
 				// Display the search bar and the table of entries
-				echo "<form method='GET' action=''>";
-				echo "<input type='text' name='search' id='search' placeholder='Search for your tasks'>";
-				echo "</form>";
+				echo "<form method='GET' action=''>\r\n";
+				echo "<input type='text' name='search' id='search' placeholder='Search for your tasks'>\r\n";
+				echo "</form>\r\n";
 				
-				echo "<table>";
-				echo "<tr><th>To Do List</th></tr>";
+				echo "<table>\r\n";
+				echo "<tr><th>To Do List</th></tr>\r\n";
 				
 				while ($row = mysqli_fetch_assoc($result)) {
                 $todo_text = $row['todo_text'];
 				
     			// Display the table row with the data
-    			echo "<tr>";
-    			echo "<td>$todo_text</td>";
-    			echo "</tr>";
+    			echo "<tr><td>$todo_text</td></tr>\r\n";
 				}
 				
 				echo "</table>";
