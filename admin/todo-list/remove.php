3c5a14e1c391fd418e381aabd2d094ece19b7504
SimDifRandomHD <lachlan@lochstudios.com>
2023-03-30 21:02:45 +1100

    Update remove.php

---
+++ admin/todo-list/remove.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>To Do List - Insert</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" href="../../images/logo.png" type="image/png" />
+</head>
+<body>
+	<header>
+		<div class="header-title">
+			<h1>To Do List</h1>
+		</div>
+		<div class="menu">
+			<button onclick="location.href='../index.php'">BACK</button>
+			<button onclick="location.href='index.php'">Home</button>
+			<button onclick="location.href='insert.php'">New</button>
+            <button onclick="location.href='update.php'">Update</button>
+            <button onclick="location.href='completed.php'">Done</button>
+			<button onclick="location.href='remove.php'">Delete</button>
+		</div>
+	</header>
+
+    <div class="container">
+        <br>
+	</div>
+</body>
+</html>
