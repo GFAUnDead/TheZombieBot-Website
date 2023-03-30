866842c0e4197d107178fc66794df9b968e30f6e
GFAUnDead <hi@simdifrandomhd.xyz>
2023-03-30 13:05:59 +1100

    Update index.php

--- admin/todo-list/index.php
+++ admin/todo-list/index.php
@@ -24,13 +24,17 @@
     <div class="container">
         <br>
         <?php
-            // Check if the API key is specified in the URL
-            if (isset($_GET['api'])) {
-                // Retrieve the API key from the URL
-                $api_key = $_GET['api'];
+            // Check if the API key was submitted via a form
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Retrieve the API key from the form data
+                $api_key = $_POST['api_key'];
             } else {
-                // Return an error message if the API key is not specified in the URL
-                echo "API key is required.";
+                // Display the form to get the API key from the user
+                echo '<form method="post">';
+                echo '<label for="api_key">API Key:</label>';
+                echo '<input type="text" id="api_key" name="api_key">';
+                echo '<input type="submit" value="Submit">';
+                echo '</form>';
                 exit();
             }
 
@@ -79,6 +83,63 @@
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
+            
+            // Retrieve the data from the table for the specified channel name
+            $sql = "SELECT todo_text FROM todos WHERE channel_id IN (SELECT
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                // Check if the API key is provided in the form data
+                if (isset($_POST['api_key'])) {
+                    // Retrieve the API key from the form data
+                    $api_key = $_POST['api_key'];
+                } else {
+                    // Return an error message if the API key is not provided in the form data
+                    echo "API key is required.";
+                    exit();
+                }
+            } else {
+                // Return an error message if the request method is not POST
+                echo "Invalid request method.";
+                exit();
+            }
+
+            // Connect to the database
+            include('db_connect.php');
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            // Prepare the SQL statement to retrieve the channel name and username for the given API key
+            $stmt = $conn->prepare("SELECT channelname FROM allowed_users WHERE api_key = ?");
+            $stmt->bind_param("s", $api_key);
+
+            // Execute the SQL statement
+            $stmt->execute();
+
+            // Bind the result to variables
+            $stmt->bind_result($channelname);
+
+            // Fetch the result
+            $stmt->fetch();
+
+            // Close the statement
+            $stmt->close();
+
+            // Close the database connection
+            $conn->close();
+
+            // Check if the provided API key is valid and retrieve the channel name from the database
+            if (empty($channelname)) {
+                // Return an error message if the API key is not valid
+                echo "Invalid API key.";
+                exit();
+            }
+
+            // Connect to the database
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
 
             // Retrieve the data from the table for the specified channel name
             $sql = "SELECT todo_text FROM todos WHERE channel_id IN (SELECT id FROM channels WHERE name=?)";
@@ -86,7 +147,7 @@
             $stmt->bind_param("s", $_GET['channel']);
             $stmt->execute();
             $result = $stmt->get_result();
-                    
+
             // Check if the query was successful
             if (!$result) {
                 // If not, log the error and display an error message to the user
@@ -94,33 +155,33 @@
                 echo "An error occurred while retrieving the data. Please try again later.";
                 exit();
             }
-            
+
             // Check if any rows were returned
             if ($result->num_rows === 0) {
                 // If not, display a message to the user
                 echo "No data found for the specified channel.";
                 exit();
             }
-            
+
             echo "<h2>Viewing all available tasks on this page for $_GET[channel]:</h2>\r\n";
-            
+
             // Display the search bar and the table of entries
             echo "<form method='GET' action=''>\r\n";
             echo "<input type='text' name='search' id='search' placeholder='Search for your tasks'>\r\n";
             echo "</form>\r\n";
-            
+
             echo "<table>\r\n";
             echo "<tr><th>To Do List</th></tr>\r\n";
-            
+
             while ($row = mysqli_fetch_assoc($result)) {
                 $todo_text = $row['todo_text'];
             
                 // Display the table row with the data
                 echo "<tr><td>$todo_text</td></tr>\r\n";
             }
-            
+
             echo "</table>";
-            
+
             $stmt->close();
             $conn->close();
 		?>
