ada1c471d5da8542d6f6d55b39a76c4458c0eca0
SimDifRandomHD <lachlan@lochstudios.com>
2023-03-30 21:06:46 +1100

    Update completed.php

---
+++ admin/todo-list/completed.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>To Do List - Insert</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" href="../../images/logo.png" type="image/png" />
+</head>
+<body>
+	<header>
+		<div class="header-title">
+			<h1>To Do List</h1>
+		</div>
+		<div class="menu">
+			<button onclick="history.back();">Home</button>
+		</div>
+	</header>
+
+    <div class="container">
+        <br>
+	</div>
+</body>
+</html>
