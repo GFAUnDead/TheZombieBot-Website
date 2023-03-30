cd4b970fc9a60a4f41444bfd0fe9d9fccb168946
SimDifRandomHD <lachlan@lochstudios.com>
2023-03-30 18:45:42 +1100

    Update insert.php

--- admin/todo-list/insert.php
+++ admin/todo-list/insert.php
@@ -24,24 +24,25 @@
         <br>
             <?php
                 // Check if the API key was submitted via a form
-                if ($_SERVER['REQUEST_METHOD'] == 'POST');
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 // Retrieve the API key from the form data
                 $api_key = $_POST['api_key'];
 
-                // Connect to the database that stores the API keys
-                $api_servername = "(REDACTED)";
-                $api_username = "(REDACTED)";
-                $api_password = "(REDACTED)";
-                $api_dbname = "(REDACTED)";
-                $api_conn = new mysqli($api_servername, $api_username, $api_password, $api_dbname);
+                // Connect to the database
+                $servername = "(REDACTED)";
+                $username = "(REDACTED)";
+                $password = "(REDACTED)";
+                $dbname = "(REDACTED)";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
 
                 // Check if the connection is successful
-                if ($api_conn->connect_error) {
-                    die("API Connection failed: " . $api_conn->connect_error);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
                 }
 
                 // Prepare the SQL statement to retrieve the channel name and username for the given API key
-                $stmt = $api_conn->prepare("SELECT channelname FROM allowed_users WHERE api_key = ?");
+                $stmt = $conn->prepare("SELECT channelname FROM allowed_users WHERE api_key = ?");
                 $stmt->bind_param("s", $api_key);
 
                 // Execute the SQL statement
@@ -57,24 +58,87 @@
                 $stmt->close();
 
                 // Close the database connection
-                $api_conn->close();
+                $conn->close();
 
                 // Check if the provided API key is valid and retrieve the channel name from the database
                 if (empty($channelname)) {
                     // Return an error message if the API key is not valid
                     echo "Invalid API key.";
-                exit();
-                } else {
-                // Display the form to get the API key from the user
-                if ($_SERVER['REQUEST_METHOD'] == 'GET' || (isset($_POST['api_key']) && !$api_key_valid));
-                echo '<form method="post">';
-                echo '<label for="api_key">API Key:</label>';
-                echo '<input type="text" id="api_key" name="api_key">';
-                echo '<input type="submit" value="Submit">';
-                echo '</form>';
-                exit();
+                    exit();
                 }
-        ?>
+                } else {
+                    // Display the form to get the API key from the user
+                    echo '<form method="post">';
+                    echo '<label for="api_key">API Key:</label>';
+                    echo '<input type="text" id="api_key" name="api_key">';
+                    echo '<input type="submit" value="Submit">';
+                    echo '</form>';
+                    exit();
+                }
+            
+                // Check if the form to add a task was submitted
+                if (isset($_POST['todo_text'])) {
+                    // Connect to the database
+                    $servername = "(REDACTED)";
+                    $username = "(REDACTED)";
+                    $password = "(REDACTED)";
+                    $dbname = "(REDACTED)";
+    
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+
+                    // Check if the connection is successful
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+    
+                    // Prepare the SQL statement to insert the task into the database
+                    $stmt = $conn->prepare("INSERT INTO todos (user_id, todo_text, completed) VALUES ((SELECT id FROM users WHERE name=?), ?, false)");
+                    $stmt->bind_param("ss", $channelname, $_POST['todo_text']);
+    
+                    // Execute the SQL statement
+                    if ($stmt->execute()) {
+                        // Display a success message to the user
+                        echo "Task added successfully!";
+                    } else {
+                        // If the SQL statement fails, display an error message to the user
+                        echo "An error occurred while adding the task. Please try again later.";
+                    }
+    
+                    // Close the statement
+                    $stmt->close();
+            
+                    // Close the database connection
+                    $conn->close();
+                }
+                // Check if the user has submitted the form to add a new task
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    // Retrieve the task text from the form data
+                    $task_text = $_POST['task_text'];
+    
+                // Connect to the database
+                include('db_connect.php');
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+    
+                // Prepare the SQL statement to insert the task into the database
+                $sql = "INSERT INTO todos (user_id, todo_text, completed) VALUES ((SELECT id FROM users WHERE name='$channelname'), ?, false)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("s", $task_text);
+    
+                // Execute the SQL statement
+                if ($stmt->execute()) {
+                        echo "Task added successfully.";
+                } else {
+                    echo "An error occurred while adding the task.";
+                }
+    
+                // Close the statement and the database connection
+                $stmt->close();
+                $conn->close();
+            }
+            ?>
 	</div>
 </body>
 </html>
